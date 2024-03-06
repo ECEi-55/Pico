e525f72bd4a7533cf1312724150bbc236a1dded5
gregk27 <git@gregk.ca>
2024-03-06 07:49:09 -0500

    Update fsm_update to change state when applicable

--- src/fsm.c
+++ src/fsm.c
@@ -40,16 +40,25 @@ void fsm_update(motor_t *motor, limit_t *upperLimit, limit_t *lowerLimit) {
     switch(_currentState) {
         case ACTIVE:
             // When empty or stowing, lower applicator until the limit is hit
-            motor_set(motor, lowerLimit->isClosed ? 0 : 1);
+            if(lowerLimit->isClosed){
+                motor_set(motor, 0);
+                _change_state(EMPTY);
+            }
+            else {
+                motor_set(motor, 1);
+            }
             break;
         case RETRACT:
             // When retracting, raise a bit
             motor_set(motor, upperLimit->isClosed ? 0 : -0.5);
-            if(++_raiseCount > RAISE_DURATION){
+            if(++_raiseCount > RAISE_DURATION || upperLimit->isClosed){
                 _change_state(IDLE);
             }
             break;
         case STOW:
+            if(upperLimit->isClosed)
+                _change_state(IDLE);
+            // Fallthrough to common logic
         case EMPTY:
             // When empty or stowing, raise applicator until at limit
             motor_set(motor, upperLimit->isClosed ? 0 : -1);
